a2cac1fe2f5346fbd51611ceef3482f375090f67
songzhikun <2303602552@qq.com>
2025-01-03 16:58:57 +0800

    放了个csv

--- task2/zhihu.py
+++ task2/zhihu.py
@@ -57,25 +57,27 @@ for _ in range(1):
     driver.execute_script(js)
     time.sleep(2)
 time.sleep(5)
-
+#拉到底 获得尽量多的问题
 question1=driver.find_elements(By.XPATH, "//h2/div/a")
 url_list=[]
 title_list=[]
 Answer=[]
 question_content=[]
+#问题的爬取
 for question in question1:
     url_list.append(question.get_attribute("href"))
     title_list.append(question.text)
 # 爬取答案
 writer.writerow(title_list)
 for url in url_list:
+    #爬多了会寄 设置少一点...
     t+=1
-    if(t==10):  
+    if(t==15):  
         break
     driver.get(f"{url}")
     time.sleep(5)
     
-    
+    #是否有展开的按钮
     try:
          stretch=driver.find_element(By.XPATH,'//div[@class="QuestionRichText QuestionRichText--expandable QuestionRichText--collapsed"]/div/button').click()
          question_content.append(driver.find_element(By.XPATH,'//div[@class="QuestionRichText QuestionRichText--expandable"]').text)
@@ -93,13 +95,13 @@ for url in url_list:
         pass
 
     for i in range(10):
-    
+        #按下上箭头 模拟用户行为 不然刷新不出来
         js_real=f"window.scrollTo(0, {i*5000});"
         driver.execute_script(js_real)
         actions.send_keys(Keys.PAGE_UP).perform()
         actions.send_keys(Keys.PAGE_DOWN).perform()
         time.sleep(1)
-        
+    #找到问题答案
     answers=driver.find_elements(By.XPATH,'//div[@class="ContentItem AnswerItem"]')
     answer_text=[element.text for element in answers]
     Answer.append(answer_text)
