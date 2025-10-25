ef0c1f11a702af8e45eecb2e6eff588650f21e64
Zhang <Laureysaldrin@gmail.com>
2025-10-25 10:47:36 +0800

    1

--- 2025/task1/Super-Hercules/Campus_IF_Love/test.py
+++ 2025/task1/Super-Hercules/Campus_IF_Love/test.py
@@ -35,40 +35,42 @@ import re
 #     print(i)
 
 
+# source = """
+# 下面是这学期要学习的课程：
 
-import re
+# <a href='https://www.bilibili.com/video/av66771949/?p=1' target='_blank'>点击这里，边看视频讲解，边学习以下内容</a>
+# 这节讲的是牛顿第2运动定律
 
-source = """
-下面是这学期要学习的课程：
+# <a href='https://www.bilibili.com/video/av46349552/?p=125' target='_blank'>点击这里，边看视频讲解，边学习以下内容</a>
+# 这节讲的是毕达哥拉斯公式
 
-<a href='https://www.bilibili.com/video/av66771949/?p=1' target='_blank'>点击这里，边看视频讲解，边学习以下内容</a>
-这节讲的是牛顿第2运动定律
+# <a href='https://www.bilibili.com/video/av90571967/?p=33' target='_blank'>点击这里，边看视频讲解，边学习以下内容</a>
+# 这节讲的是切割磁力线
+# """
 
-<a href='https://www.bilibili.com/video/av46349552/?p=125' target='_blank'>点击这里，边看视频讲解，边学习以下内容</a>
-这节讲的是毕达哥拉斯公式
+# # 替换函数，参数是 Match对象
+# def subfunc(match):
+#     # Match对象 的 group(0) 返回的是整个匹配上的字符串。
+#     src = match.group(0)
 
-<a href='https://www.bilibili.com/video/av90571967/?p=33' target='_blank'>点击这里，边看视频讲解，边学习以下内容</a>
-这节讲的是切割磁力线
-"""
+#     # Match对象 的 group(1) 返回的是第一个group分组的内容。
+#     # number = int(match.group(1)) + 6
+#     dest = f"/av{int(match.group(1)) + 6}/"
 
-# 替换函数，参数是 Match对象
-def subfunc(match):
-    # Match对象 的 group(0) 返回的是整个匹配上的字符串。
-    src = match.group(0)
+#     print(f'{src} 替换为 {dest}')
 
-    # Match对象 的 group(1) 返回的是第一个group分组的内容。
-    # number = int(match.group(1)) + 6
-    dest = f"/av{int(match.group(1)) + 6}/"
+#     # 返回值就是最终替换的字符串
+#     return dest
 
-    print(f'{src} 替换为 {dest}')
+# newStr = re.sub(r"/av(\d+)/", subfunc , source)
+# print(newStr)
 
-    # 返回值就是最终替换的字符串
-    return dest
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
 
-newStr = re.sub(r"/av(\d+)/", subfunc , source)
-print(newStr)
-
-
+for char in reverse('golf'):
+    print(char)
 
 
 
